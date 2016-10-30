3a674810a22277c55c52f31bd826b554a76a2f7d
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2016-10-30 16:16:44 +0100

    used context.processor to pass variable to template

--- goat_bot_main.py
+++ goat_bot_main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import requests
-import json
 
 
 goat_bot = Flask(__name__)
@@ -10,11 +9,15 @@ def get_gif_by_tag(tag):
     request = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={}&fmt=json'.format(tag))
     return request.json()['data']['image_url']
 
+@goat_bot.context_processor
+def get_goat():
+    gif_goat = get_gif_by_tag("goat")
+    return dict(gif=gif_goat)
+
 @goat_bot.route('/')
 def inspiration_goat():
-    gif = get_gif_by_tag("goat")
     return render_template('youve_goat_this.html')
 
+
 if __name__ == '__main__':
-    # print(get_gif_by_tag("goat"))
     goat_bot.run()
