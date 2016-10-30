0b478a9c91b4bc81dd9a40b98e98e5d0127c0d8a
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2016-10-30 14:49:19 +0100

    request random goat by tag

--- goat_bot_main.py
+++ goat_bot_main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 
 goat_bot = Flask(__name__)
 
 
+def get_gif_by_tag(tag):
+    request = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={}&fmt=json'.format(tag))
+    return request.json()['data']['image_url']
+
 @goat_bot.route('/')
-def hello_world():
-    return "Hello, World!"
+def inspiration_goat():
+    gif = get_gif_by_tag("goat")
+    return render_template('youve_goat_this.html')
 
 if __name__ == '__main__':
+    # print(get_gif_by_tag("goat"))
     goat_bot.run()
